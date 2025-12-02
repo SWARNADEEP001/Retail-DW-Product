e90491d40db829b5bac492647803210ffee71ad4
SWARNADEEP001 <deysarkar.riju1@gmail.com>
2025-12-02 23:34:06 +0530

    Added main.sql for GitHub Language Detection

---
+++ main.sql
@@ -0,0 +1,7 @@
+-- Entry point for Data Warehouse scripts
+
+SOURCE sql/01_create_schema.sql;
+SOURCE sql/02_create_tables.sql;
+SOURCE sql/03_load_data.sql;
+SOURCE sql/04_create_views.sql;
+SOURCE sql/05_kpis.sql;
